2958def550105265c6a0e6d99013cd7740c22219
Liam Du Preez <ldu-pree@c3r6s9.wethinkcode.co.za>
2019-05-22 10:20:21 +0200

    updated some files

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,36 @@
 /*   By: ldu-pree <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 11:46:33 by ldu-pree          #+#    #+#             */
-/*   Updated: 2019/05/21 16:21:11 by ldu-pree         ###   ########.fr       */
+/*   Updated: 2019/05/22 09:16:29 by ldu-pree         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int		loop;
-	char	zero;
-	int		val;
+	int neg;
+	int num;
+	int i;
 
-	loop = 0;
-	zero = 'p';
-	val = 0;
-	while (str[loop] == ' ' || str[loop] == '\t')
-		loop++;
-	if (str[loop] == '-')
-		zero = 'n';
-	if (str[loop] == '+')
-		loop++;
-	while (ft_isdigit(str[loop]))
+	i = 0;
+	neg = 1;
+
+	num = 0;
+	while (str[i] <= ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		val *= 10;
-		val += (str[loop] - 48);
-		loop++;
+		if (str[i] == '-')
+		{
+			neg *= -1;
+		}
+		i++;
 	}
-	return (val);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - 48);
+		i++;
+	}
+	return (num * neg);
 }
