50d14d4fc0109019dda2027ef01aaf9090406a13
Liam Du Preez <ldu-pree@c3r6s3.wethinkcode.co.za>
2019-05-23 10:44:41 +0200

    Normed all of the functions

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,24 +6,24 @@
 /*   By: ldu-pree <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:01:02 by ldu-pree          #+#    #+#             */
-/*   Updated: 2019/05/22 11:13:56 by ldu-pree         ###   ########.fr       */
+/*   Updated: 2019/05/23 10:42:05 by ldu-pree         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    if (nb < 0)
-    {
-        nb *= -1;
+	if (nb < 0)
+	{
+		nb *= -1;
 		ft_putchar('-');
-    }
-    if (nb >= 10)
-    {
-        ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
-    }
-    else
-        ft_putchar(nb + '0');
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
 }
