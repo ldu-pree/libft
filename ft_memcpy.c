f7d9837efe943497623561a6ad981d34f7761660
Liam Du Preez <ldu-pree@c3r6s3.wethinkcode.co.za>
2019-05-27 09:16:22 +0200

    remove .o

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,19 +6,21 @@
 /*   By: ldu-pree <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 17:08:49 by ldu-pree          #+#    #+#             */
-/*   Updated: 2019/05/23 18:20:39 by ldu-pree         ###   ########.fr       */
+/*   Updated: 2019/05/24 11:05:26 by ldu-pree         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *s1, const void *s2, size_t n)
+void	*ft_memcpy(void *restrict s1, const void *restrict s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
+		if (s1 == '\0' && s2 == '\0')
+			break;
 		((char*)s1)[i] = ((char*)s2)[i];
 		i++;
 	}
